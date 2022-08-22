de60734878772883245d0bfaf40d0346ba6f8d3e
maltexade1 <40648156+maltexade1@users.noreply.github.com>
2022-08-22 22:17:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import json
 import imdb
 import os
 PORT = int(os.environ.get('PORT', 5000))
-api_key= 5445423130:AAHxkRrbn8wSYbeaJteB4QCAKIu_bY7r6Xo
+api_key= 5312568700:AAGaBlpKz9NcEmxvM-jymJtgIrs6o2EB9sI
 ia = imdb.IMDb() 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
